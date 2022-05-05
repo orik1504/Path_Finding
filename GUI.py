e934d824bcaf1b351cc68ca1c22a7240cc739898
orik1504 <orik1504@gmail.com>
2022-05-06 01:00:51 +0300

    created gui class

--- GUI.py
+++ GUI.py
@@ -4,3 +4,15 @@ from board import Grid
 from board import Node
 import math
 
+class Gui_board(Grid):
+    """ This class will create a board that the user will interact with
+     will place blocking pathes, start point and end point
+
+        0 - white -> blank cell 
+        1 - black -> border/ blocking path
+        2 - green -> open neighbors
+        3 - red -> closed neighbors
+        4 - orange -> end point
+        5 - blue -> start point
+        6 - purple -> the shortest path
+    """
